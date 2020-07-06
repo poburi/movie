38ade06c0e9ff8d3c603de4c289b29030be57be1
hanheesong <gmlthd0908@gmail.com>
2020-07-06 21:45:58 +0900

    Updates

---
+++ precache-manifest.686766406347ebc13bded32a5e3f479b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3ad7aefff8d30cb45cd7331be0cf71",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "84687b0461dacc918b74",
+    "url": "/movie/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e80187f9cd3aac4909c",
+    "url": "/movie/static/js/2.333d603e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie/static/js/2.333d603e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84687b0461dacc918b74",
+    "url": "/movie/static/js/main.59dc4f3c.chunk.js"
+  },
+  {
+    "revision": "ed84cc0093c15cee011f",
+    "url": "/movie/static/js/runtime-main.2bc06d1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie/static/media/logo.5d5d9eef.svg"
+  }
+]);
